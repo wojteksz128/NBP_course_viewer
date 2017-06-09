7dd7c5d47150724d4e39ddb930932e6737a1130b
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-09 15:33:11 +0200

    XMLFilesDownloader added.

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -5,8 +5,6 @@ package pl.parser.nbp;
  */
 public class MainClass {
 
-    public static final String EXCHANGE_RATE_URL_PART =  "http://www.nbp.pl/kursy/xml/";
-
     public static void main(String[] args) {
 
     }
---
+++ src/main/java/pl/parser/nbp/util/XMLFilesDownloader.java
@@ -0,0 +1,13 @@
+package pl.parser.nbp.util;
+
+/**
+ * Created by Wojciech Szczepaniak on 09.06.2017.
+ */
+public class XMLFilesDownloader {
+
+    public static final String EXCHANGE_RATE_URL_PART =  "http://www.nbp.pl/kursy/xml/";
+    public static final String DIR_FILENAME = "dir";
+    public static final String DIR_EXTENSION = ".txt";
+
+
+}
