463e9964da09df6c1842959b7286646fede59e12
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-09 15:06:30 +0200

    DayCurrency model without getters added.

---
+++ src/main/java/pl/parser/nbp/model/DayCurrency.java
@@ -0,0 +1,15 @@
+package pl.parser.nbp.model;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Wojciech Szczepaniak on 09.06.2017.
+ */
+public class DayCurrency {
+
+    private String name;
+    private BigDecimal converter;
+    private String code;
+    private BigDecimal purchaseRate;
+    private BigDecimal sellingRate;
+}
