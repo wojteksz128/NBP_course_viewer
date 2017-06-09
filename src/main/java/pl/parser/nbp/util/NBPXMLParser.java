834d3dd18012bfd879f8411c2497a1d481dda17a
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-09 23:42:33 +0200

    Add empty method inside NBPXMLParser.

---
+++ src/main/java/pl/parser/nbp/util/NBPXMLParser.java
@@ -0,0 +1,20 @@
+package pl.parser.nbp.util;
+
+import org.xml.sax.XMLReader;
+import pl.parser.nbp.model.ExchangeRateTable;
+
+import java.beans.XMLDecoder;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by Wojciech Szczepaniak on 09.06.2017.
+ */
+public class NBPXMLParser {
+
+    public static List<ExchangeRateTable> getDataFromXMLs(List<URL> xmls) {
+
+
+        return null;
+    }
+}
