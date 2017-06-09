931cc2c67128e46b01bb3344a07051952074d2ad
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-09 23:02:52 +0200

    Getting file names from dir files implemented.

--- src/main/java/pl/parser/nbp/util/XMLFilesDownloader.java
+++ src/main/java/pl/parser/nbp/util/XMLFilesDownloader.java
@@ -1,32 +1,51 @@
 package pl.parser.nbp.util;
 
-import java.io.File;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Year;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Wojciech Szczepaniak on 09.06.2017.
  */
 public class XMLFilesDownloader {
 
-    private static final String EXCHANGE_RATE_URL_PART =  "http://www.nbp.pl/kursy/xml/";
+    private static final String EXCHANGE_RATE_URL_PART = "http://www.nbp.pl/kursy/xml/";
     private static final String DIR_FILENAME = "dir";
     private static final String DIR_EXTENSION = ".txt";
 
-    public static List<File> downloadXMLsBetweenDates(LocalDate beginDate, LocalDate endDate) throws MalformedURLException {
-        List<File> files = downloadDirFilesWithFileNames(beginDate, endDate);
-
-        System.out.println(files);
+    public static List<File> downloadXMLsBetweenDates(LocalDate beginDate, LocalDate endDate) throws IOException {
+        List<String> fileNames = getFileNames(downloadDirFilesWithFileNames(beginDate, endDate), beginDate, endDate);
 
         return null;
     }
 
-    private static List<File> downloadDirFilesWithFileNames(LocalDate beginDate, LocalDate endDate) throws MalformedURLException {
-        List<File> files = new ArrayList<>();
+    private static List<String> getFileNames(List<URL> dirFiles, LocalDate beginDate, LocalDate endDate) throws IOException {
+        List<String> fileNames = new ArrayList<>();
+
+        for (URL dirFile : dirFiles) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(dirFile.openStream()));
+
+            fileNames.addAll(br.lines().parallel().filter(str -> {
+                if (!str.startsWith("c")) {
+                    return false;
+                }
+                LocalDate date = LocalDate.parse(str.substring(5, 11), DateTimeFormatter.ofPattern("yyMMdd"));
+                return (date.isAfter(beginDate) || date.isEqual(beginDate)) && (date.isBefore(endDate) || date.isEqual(endDate));
+            }).collect(Collectors.toList()));
+        }
+
+        System.err.println(fileNames);
+        return fileNames;
+    }
+
+    private static List<URL> downloadDirFilesWithFileNames(LocalDate beginDate, LocalDate endDate) throws MalformedURLException {
+        List<URL> files = new ArrayList<>();
 
         if (beginDate.isAfter(endDate)) {
             throw new IllegalArgumentException("Begin date (" + beginDate + ") is after end date (" + endDate + ").");
@@ -43,8 +62,7 @@ public class XMLFilesDownloader {
         Year currYear = Year.from(beginDate);
 
         while (!currYear.isAfter(Year.from(endDate))) {
-            URL url = new URL(EXCHANGE_RATE_URL_PART + DIR_FILENAME + (currYear.equals(Year.now()) ? "" : currYear) + DIR_EXTENSION);
-            files.add(new File(url.getFile()));
+            files.add(new URL(EXCHANGE_RATE_URL_PART + DIR_FILENAME + (currYear.equals(Year.now()) ? "" : currYear) + DIR_EXTENSION));
             currYear = currYear.plusYears(1);
         }
 
